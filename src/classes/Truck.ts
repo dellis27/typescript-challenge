8924867e8e54799354e607ab887f307a0bfc7e1c
dellis27 <cooperdellis2772@gmail.com>
2025-02-17 23:28:21 -0600

    almost done

--- src/classes/Truck.ts
+++ src/classes/Truck.ts
@@ -5,7 +5,7 @@ import Car from './Car.js';
 import Wheel from './Wheel.js';
 import AbleToTow from '../interfaces/AbleToTow.js';
 // TODO: The Truck class should extend the Vehicle class and should implement the AbleToTow interface
-class Truck extends Vehicle {
+class Truck extends Vehicle implements AbleToTow{
   vin: string;
   color: string;
   make: string;
@@ -35,14 +35,15 @@ class Truck extends Vehicle {
     this.year = year;
     this.weight = weight;
     this.topSpeed = topSpeed;
-    if (wheels.length !>= 4){
+    this.towingCapacity = towingCapacity;
+    if (wheels.length < 4){
       this.wheels = [new Wheel(), new Wheel(), new Wheel(), new Wheel()];
 
     } else {
       this.wheels = wheels;
 
     }
-    this.towingCapacity = towingCapacity;
+    
   }
 
   // TODO: Declare properties of the Truck class
@@ -55,11 +56,12 @@ class Truck extends Vehicle {
     // TODO: The constructor should check if the wheels array has 4 elements and create 4 new default Wheel objects if it does not
 
   // TODO: Implement the tow method from the AbleToTow interface
+  
   tow(vehicle: Truck | Motorbike | Car): void {
-    console.log(`The Make of the vehicle is ${this.make}`);
-    console.log(`The Model of the car is ${this.model}`);
+    console.log(`The Make of the vehicle is ${vehicle.make}`);
+    console.log(`The Model of the car is ${vehicle.model}`);
 
-    if (this.weight < towingCapacity) {
+    if (vehicle.weight < this.towingCapacity) {
       console.log('The vehicle is being towed');
     } else {
       console.log('The vehicle is too heavy to be towed');
@@ -71,6 +73,31 @@ class Truck extends Vehicle {
   }
 
   // TODO: Override the printDetails method from the Vehicle class
+  override printDetails(): void {
+    super.printDetails();
+
+    console.log(`VIN: ${this.vin}`);
+    console.log(`Color: ${this.color}`);
+    console.log(`Make: ${this.make}`);
+    console.log(`Model: ${this.model}`);
+    console.log(`Year: ${this.year}`);
+    console.log(`Weight: ${this.weight} lbs`);
+    console.log(`Top Speed: ${this.topSpeed} mph`);
+    console.log(`Towing Capacity:  ${this.towingCapacity}`)
+
+    console.log(
+      `Wheel 1: ${this.wheels[0].getDiameter} inch with a ${this.wheels[0].getTireBrand} tire`
+    );
+    console.log(
+      `Wheel 2: ${this.wheels[1].getDiameter} inch with a ${this.wheels[1].getTireBrand} tire`
+    );
+    console.log(
+      `Wheel 3: ${this.wheels[2].getDiameter} inch with a ${this.wheels[2].getTireBrand} tire`
+    );
+    console.log(
+      `Wheel 4: ${this.wheels[3].getDiameter} inch with a ${this.wheels[3].getTireBrand} tire`
+    );
+  }
     // TODO: The method should call the printDetails method of the parent class
     // TODO: The method should log the details of the Truck
     // TODO: The details should include the VIN, make, model, year, weight, top speed, color, towing capacity, and wheels
